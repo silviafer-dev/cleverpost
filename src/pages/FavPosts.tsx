b927d8070c697640ff52810d9c8b8c535a7cb143
Silvia Ferrai <silviafer@hotmail.it>
2022-11-01 00:08:19 +0100

    Add style to favorite page

--- src/pages/FavPosts.tsx
+++ src/pages/FavPosts.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useAppSelector } from "../app/hooks";
+import { useAppSelector, useAppDispatch } from "../app/hooks";
 import { AuthStatus } from "../components/auth";
-import { selectFavoriteState } from "../features/post/favSlices";
+import { removePost, selectFavoriteState } from "../features/post/favSlices";
+import { GiBrokenHeart } from "react-icons/gi";
+import "../sass/favPost.scss";
 
 export function FavPosts(props: {
   auth: boolean;
   setAuth: (state: boolean) => void;
 }) {
   const favPosts = useAppSelector(selectFavoriteState);
+  const dispatch = useAppDispatch();
+
+  const removeFromFavorite = (id: string | number) => {
+    dispatch(removePost(id));
+  };
   return (
     <div>
       <div className="header">
@@ -25,10 +32,10 @@ export function FavPosts(props: {
         </div>
       </div>
 
-      <div>
+      <div className="fav-container">
         {favPosts.map((fav) => (
-          <div key={fav.id}>
-            <h3>
+          <div key={fav.id} className=" fav">
+            <h3 className="fav__author">
               {fav.userId === 1
                 ? "Ian Flamming"
                 : "" || fav.userId === 2
@@ -51,8 +58,9 @@ export function FavPosts(props: {
                 ? "Aldous Huxley"
                 : ""}
             </h3>
-            <h4>{fav.title}</h4>
-            <p>{fav.body}</p>
+            <h4 className="fav__title">{fav.title}</h4>
+            <p className="fav__body">{fav.body}</p>
+            <GiBrokenHeart className="broken-heart" onClick={() => removeFromFavorite(fav.id)} />
           </div>
         ))}
       </div>
